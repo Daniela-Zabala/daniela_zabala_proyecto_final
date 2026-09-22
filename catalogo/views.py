65dc461643f9325d5a3363f0aba8bfbd6b95ae4c
Daniela-Zabala <danielazabala9mm@gmail.com>
2026-09-22 21:40:36 +0200

    crear otras views de techcovers

--- catalogo/views.py
+++ catalogo/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, 'inicio.html')
+
+def catalogo(request):
+    return render(request, 'catalogo.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
