235d8753d65f0a733575b349ef73ea22bde61d69
Daniela-Zabala <danielazabala9mm@gmail.com>
2026-09-23 12:36:29 +0200

    subir imagen de imagen

--- daniela_zabala_proyecto_final_core/urls.py
+++ daniela_zabala_proyecto_final_core/urls.py
@@ -23,4 +23,6 @@ from catalogo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.inicio, name='inicio'),
+    path('catalogo/', views.catalogo, name='catalogo'),
+    path('contacto/', views.contacto, name='contacto'),
 ]
